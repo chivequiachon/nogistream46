9841bda1ee4f2648696988ff59ff9e7d60069322
chivequiachon <chivequiachon@gmail.com>
2019-10-13 01:35:05 +0800

    For testing

--- stream_service/urls.py
+++ stream_service/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.homepage, name='homepage'),
+    url(r'^home/$', views.homepage, name='homepage'),
     url(r'^view/$', views.sample_viewpage, name='sample_viewpage'),
 ]
